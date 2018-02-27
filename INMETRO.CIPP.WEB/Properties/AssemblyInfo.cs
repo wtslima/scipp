62daf21f6bb7c47f12da7e6c998bfe391723c5e7
wtslima <wtslima@gmail.com>
2018-02-27 14:36:38 -0300

    Correções Assembly Version

--- INMETRO.CIPP.WEB/Properties/AssemblyInfo.cs
+++ INMETRO.CIPP.WEB/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c895f720-2570-4fee-8609-db26b2326896")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.18863")]
+[assembly: AssemblyFileVersion("1.0.0.18863")]
